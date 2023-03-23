79b20e0bc1d39783430d5e845f9f9975ea70948a
michaluReuters <111868860+michaluReuters@users.noreply.github.com>
2023-03-23 12:58:09 +0100

    Adds Commented Update S3 Object Metadata

--- env-sh-file-normalise-metadata-extraction/lambda.py
+++ env-sh-file-normalise-metadata-extraction/lambda.py
@@ -1,3 +1,5 @@
+import json
+
 from aws_lambda_powertools import Logger
 from model import FFProbe, FFprobeDTO
 
@@ -6,7 +8,8 @@ import boto3
 
 logger = Logger()
 s3client = boto3.resource("s3")
-
+client = boto3.client('events')
+LAMBDA_NAME = os.environ["AWS_LAMBDA_FUNCTION_NAME"]
 
 @logger.inject_lambda_context(log_event=True)
 def handler(event, context):
@@ -23,9 +26,9 @@ def handler(event, context):
             logger.error(f"There was an error while fetching {event_object}. Exception is: {exc}")
 
         try:
+            # Extracting Metadata
             metadata = FFProbe(video_file=local_file_name)
             logger.info(f'METADATA STREAM = {metadata.show_json_format()}')
-
             result = FFprobeDTO()
             result.gather_required_data(metadata)
             logger.info(f'DATA SEPARATED = {result.data}')
@@ -33,9 +36,35 @@ def handler(event, context):
 
             os.system(f"rm /tmp/{event_object}")
             logger.info(f"Result Data: {result.destination_data}")
+
+            #TODO FOR TEST PURPOSES METHOD BELLOW IS COMMENTED
+
+            # # Replacing Object Metadata
+            # s3client.copy_object(
+            #     Bucket=event_bucket,
+            #     CopySource={'Bucket': event_bucket, 'Key': event_object},
+            #     Key=event_object,
+            #     Metadata=result.destination_data,
+            #     MetadaDirective='REPLACE'
+            # )
+
+            # Sending to EventBridge
+            data_to_send = json.dumps(result.destination_data)
+            response = client.put_events(
+                Entries=[
+                    {
+                        'Source': f"{LAMBDA_NAME}-complete",
+                        'Resources': [LAMBDA_NAME],
+                        'DetailType': "file-normalise-metadata-extraction-complete",
+                        'Detail': data_to_send
+                    },
+                ]
+            )
+
+            logger.info(response)
+
             return result.destination_data
         except Exception as exc:
             logger.error(f"There was an error while gathering metadata from ffprobe. Exception is: {exc}")
-    # TODO Also we would need to modify the s3 object metadata with one gathered from this step 17/03/2023
     except Exception as exc:
         logger.info(f"There has been exception. Exception is: {exc}")
